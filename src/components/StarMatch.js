dbd1c6c19bb3f362779fcf7099c786267a8fa03a
Sean Leonard <seanleonardsap@gmail.com>
2021-02-27 11:00:34 +0000

    button click events, state changes

--- src/components/StarMatch.js
+++ src/components/StarMatch.js
@@ -13,7 +13,7 @@ const PlayNumber = (props) => (
   <button
     className="number"
     style={{ backgroundColor: colors[props.status] }}
-    onClick={() => console.log("Num", props.number)}
+    onClick={() => props.onClick(props.number, props.status)}
   >
     {props.number}
   </button>
@@ -24,7 +24,7 @@ const StarMatch = () => {
   //Random stars
   const [stars, setStars] = useState(utils.random(1, 9));
   const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
-  const [candidateNums, setCanditateNums] = useState([]);
+  const [candidateNums, setCandidateNums] = useState([]);
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
 
@@ -36,6 +36,28 @@ const StarMatch = () => {
       return candidatesAreWrong ? "wrong" : "candidate";
     }
     return "available";
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus == "used") {
+      return;
+    }
+
+    const newCandidateNums =
+      currentStatus === "available"
+        ? candidateNums.concat(number)
+        : candidateNums.filter((cn) => cn !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        (n) => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
   };
 
   return (
@@ -55,6 +77,7 @@ const StarMatch = () => {
               key={number}
               status={numberStatus(number)}
               number={number}
+              onClick={onNumberClick}
             />
           ))}
         </div>
